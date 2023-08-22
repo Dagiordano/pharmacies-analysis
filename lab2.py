418b7a6904685216733fd26198f565ebf24cf68b
Diego Giordano <diegogiordano@Diegos-MacBook-Air.local>
2023-08-22 19:27:12 -0400

    init y call de Summary

--- lab2.py
+++ lab2.py
@@ -1,28 +1,62 @@
 import requests 
-r = requests.get("https://andreshoward.com/pharmacies")
-
-diccionarios= r.json()
-
-a = diccionarios
 
 
 
 
-class Request():
-    self.url = "https://andreshoward.com/pharmacies"
+
+class Request:
     def __init__(self):
+        self.url = "https://andreshoward.com/pharmacies"
         pass
     
     def get(self):
-        solicitud = requests.get(url)
+        solicitud = requests.get(self.url)
         lista = solicitud.json()
         return lista
     
 class Sumary:
-    __data__ = ()
-    __frequencies = {}
+    def __init__(self, lista):
+        self.__data__ = lista
+        self.__frequencies__ = {}
+    
+    def call(self):
+        for i in self.__data__:
+            nombre_farmacia = i['local_nombre']
+            self.__frequencies__[nombre_farmacia]=0
+        return self.__frequencies__
     
     
+
+
+
+r = requests.get("https://andreshoward.com/pharmacies")
+
+lista = r.json()
+
+chica =lista[0]
+
+
+suma= Sumary(lista)
+
+suma.call()
+
+
+        
+        
+    
+
+        
+        
+
+
+
+        
+        
+
+    
+
+
+
         
         
         
